1ccc0d11757b2d2d282a2ecc8cceb29a65809483
Dennis Björkvall <dennis.bjorkvall@gmail.com>
2017-10-14 00:56:00 +0200

    awesome pawsome graphiql

--- api/app.js
+++ api/app.js
@@ -1,7 +1,13 @@
-const http  = require('http');
+const express = require('express');
 const postgraphql = require('postgraphql').postgraphql;
 const config = require('./config');
 
-http.createServer(
-    postgraphql(`postgres://${config.db.user}:${config.db.pass}@${config.db.host}:${config.db.port}/${config.db.schema}`)
-).listen(3000);
+const app = express();
+
+app.use(postgraphql(`postgres://${config.db.user}:${config.db.pass}@${config.db.host}:${config.db.port}/${config.db.schema}`, {
+    graphiql: true,
+    graphqlRoute: '/nippe/graphql',
+    graphiqlRoute: '/nippe/graphiql'
+}));
+
+app.listen(3000);
