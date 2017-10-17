d9bd45881240fce5676455ff9a78c0fd93e4a12a
Dennis Björkvall <dennis.bjorkvall@gmail.com>
2017-10-17 13:41:52 +0200

    Update database schema and seed

--- db/db.sql
+++ db/db.sql
@@ -1,5 +1,35 @@
-CREATE TABLE articles (
-    id character varying(36) NOT NULL,
+CREATE TABLE brands (
+    id SERIAL PRIMARY KEY,
+    name character varying(255)
+);
+
+CREATE TABLE authors (
+    id SERIAL PRIMARY KEY,
     name character varying(255),
-    data text
-)
+    brand_id int REFERENCES brands(id)
+);
+
+CREATE TABLE articles (
+    id SERIAL PRIMARY KEY,
+    name character varying(255),
+    heading character varying(255)
+    preamble text,
+    text text,
+    created_at timestamp DEFAULT now(),
+    updated_at timestamp DEFAULT now()
+    author_id int REFERENCES authors(id)
+);
+
+-- Add brands
+INSERT INTO brands (name) VALUES ('Brand1');
+INSERT INTO brands (name) VALUES ('Brand2');
+
+-- Add authors
+INSERT INTO authors (name, brand_id) VALUES ('Name1', 1);
+INSERT INTO authors (name, brand_id) VALUES ('Name2', 2);
+
+-- Add articles
+INSERT INTO articles (name, heading, preamble, text, author_id) VALUES ('test1', 'text1', 1);
+INSERT INTO articles (name, heading, preamble, text, author_id) VALUES ('test2', 'text2', 2);
+INSERT INTO articles (name, heading, preamble, text, author_id) VALUES ('test3', 'text3', 1);
+INSERT INTO articles (name, heading, preamble, text, author_id) VALUES ('test4', 'text4', 2);
