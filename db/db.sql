609c96d70a61a2d7c911beaeaa3c67c4c7eae061
Tommy *twl* Ljungberg <tommy.ljungberg@bbm.bonnier.se>
2017-10-17 16:37:09 +0200

    added images to articles

--- db/db.sql
+++ db/db.sql
@@ -17,7 +17,8 @@ CREATE TABLE articles (
     text text,
     created_at timestamp DEFAULT now(),
     updated_at timestamp DEFAULT now(),
-    author_id int REFERENCES authors(id)
+    author_id int REFERENCES authors(id),
+    image character varying(255),
 );
 
 -- Add brands
@@ -29,10 +30,10 @@ INSERT INTO authors (name, brand_id) VALUES ('Name1', 1);
 INSERT INTO authors (name, brand_id) VALUES ('Name2', 2);
 
 -- Add articles
-INSERT INTO articles (name, heading, preamble, text, author_id) VALUES ('test1', 'heading1', 'preamble1', 'text1', 1);
-INSERT INTO articles (name, heading, preamble, text, author_id) VALUES ('test2', 'heading2', 'preamble2', 'text2', 2);
-INSERT INTO articles (name, heading, preamble, text, author_id) VALUES ('test3', 'heading3', 'preamble3', 'text3', 1);
-INSERT INTO articles (name, heading, preamble, text, author_id) VALUES ('test4', 'heading4', 'preamble4', 'text4', 2);
+INSERT INTO articles (name, heading, preamble, text, author_id, image) VALUES ('test1', 'heading1', 'preamble1', 'text1', 1, 'http://img2.tvtome.com/i/u/97bf577dcc3d41a6c129b83cafb9b801.jpg');
+INSERT INTO articles (name, heading, preamble, text, author_id, image) VALUES ('test2', 'heading2', 'preamble2', 'text2', 2, 'https://static.stereogum.com/uploads/2017/01/Slayer-1485351270-compressed.jpg');
+INSERT INTO articles (name, heading, preamble, text, author_id, image) VALUES ('test3', 'heading3', 'preamble3', 'text3', 1, 'https://raw.githubusercontent.com/jameswyse/pied-piper/master/docs/images/pied-piper.png');
+INSERT INTO articles (name, heading, preamble, text, author_id, image) VALUES ('test4', 'heading4', 'preamble4', 'text4', 2, 'http://monkeyswing.se/wp-content/uploads/2017/03/SE_NOCCO_Caribbean_Herobild_170321-1280x640.jpg');
 
 -- Add search function
 CREATE FUNCTION search_articles(search text) RETURNS SETOF articles AS $$
