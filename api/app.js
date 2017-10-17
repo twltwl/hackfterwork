799877cc58625f4043e9ce24f4366ac328c3518e
Dennis Björkvall <dennis.bjorkvall@gmail.com>
2017-10-17 14:41:58 +0200

    Fix inifinit loading

--- api/app.js
+++ api/app.js
@@ -4,21 +4,22 @@ const config = require('./config');
 const routes = require('./routes');
 const app = new koa();
 const koaBody = require('koa-body');
-app.use(koaBody());
 
 app.use(async(ctx, next)=> {
     ctx.set('Access-Control-Allow-Origin', '*')
     ctx.set('Access-Control-Allow-Methods', 'GET, POST')
     ctx.set('Access-Control-Allow-Headers', 'Content-Type')
     await next();
-})
-app.use(routes.routes());  
+});
 
 app.use(postgraphql(`postgres://${config.db.user}:${config.db.pass}@${config.db.host}:${config.db.port}/${config.db.schema}`, {
     graphiql: true,
     graphqlRoute: '/nippe/graphql',
     graphiqlRoute: '/nippe/graphiql'
 }));
+
+app.use(koaBody());
+app.use(routes.routes());
 
 app.listen(3000, () => {
     console.log('listening on port: 3000')
