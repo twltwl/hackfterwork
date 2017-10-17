77b4a16567ea45d62c6f4d6485357263af3f16ac
Dennis Björkvall <dennis.bjorkvall@gmail.com>
2017-10-17 14:07:42 +0200

    Fixes for database seed

--- db/db.sql
+++ db/db.sql
@@ -12,11 +12,11 @@ CREATE TABLE authors (
 CREATE TABLE articles (
     id SERIAL PRIMARY KEY,
     name character varying(255),
-    heading character varying(255)
+    heading character varying(255),
     preamble text,
     text text,
     created_at timestamp DEFAULT now(),
-    updated_at timestamp DEFAULT now()
+    updated_at timestamp DEFAULT now(),
     author_id int REFERENCES authors(id)
 );
 
@@ -29,7 +29,7 @@ INSERT INTO authors (name, brand_id) VALUES ('Name1', 1);
 INSERT INTO authors (name, brand_id) VALUES ('Name2', 2);
 
 -- Add articles
-INSERT INTO articles (name, heading, preamble, text, author_id) VALUES ('test1', 'text1', 1);
-INSERT INTO articles (name, heading, preamble, text, author_id) VALUES ('test2', 'text2', 2);
-INSERT INTO articles (name, heading, preamble, text, author_id) VALUES ('test3', 'text3', 1);
-INSERT INTO articles (name, heading, preamble, text, author_id) VALUES ('test4', 'text4', 2);
+INSERT INTO articles (name, heading, preamble, text, author_id) VALUES ('test1', 'heading1', 'preamble1', 'text1', 1);
+INSERT INTO articles (name, heading, preamble, text, author_id) VALUES ('test2', 'heading2', 'preamble2', 'text2', 2);
+INSERT INTO articles (name, heading, preamble, text, author_id) VALUES ('test3', 'heading3', 'preamble3', 'text3', 1);
+INSERT INTO articles (name, heading, preamble, text, author_id) VALUES ('test4', 'heading4', 'preamble4', 'text4', 2);
